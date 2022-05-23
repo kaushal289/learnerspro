00aaebff54f5dedded593501661d8ce4a7f4e0d0
kaushal289 <khanalkaushal9@gmail.com>
2022-05-23 14:05:07 +0545

    commit

--- teacher/tests.py
+++ teacher/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.test import SimpleTestCase,TestCase,Client
+from django.urls import reverse,resolve
+from student.views import *
+
+
+class TestUrls(SimpleTestCase):
+    def test_case_index_url(self):
+        url=reverse('teacherregister')
+        self.assertEqual(resolve(url).func,register)
+
+
+class TestViews(TestCase):
+    def setUp(self):
+        self.client=Client()
+
+    def test_register_GET(self):
+        response=self.client.get(reverse('teacherregister'))
+        self.assertEquals(response.status_code,200)
+        self.assertTemplateUsed(response,'reglogin/teacherregistration.html')
+
