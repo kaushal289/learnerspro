d78b8c89cbd10dbb450fbac2c34a90a885aaa7b6
kaushal289 <khanalkaushal9@gmail.com>
2022-06-02 16:32:28 +0545

    changes

--- student/urls.py
+++ student/urls.py
@@ -6,10 +6,5 @@ urlpatterns = [
     path('studentregister',views.register, name="studentregister"),
     path('studentlogin',views.studentlogin,name="studentlogin"),
     path('logout',views.logout,name='logout'),
-<<<<<<< HEAD
     path('studentsubject', views.studentsubject, name="studentsubject"),
 ]
-=======
-]
-
->>>>>>> 91b04a5412e80f3a98e0d45fe4c4e84be1774a7d
--- teacher/views.py
+++ teacher/views.py
@@ -16,7 +16,6 @@ def register(request):
 
 def teacherdashboard(request):
     return render(request,"teacher/landingpage.html")
-<<<<<<< HEAD
 
 def teachersubject(request):
     return render(request, "teacher/teachersubject.html")
@@ -43,5 +42,3 @@ def addsubject(request):
     return render(request, 'addsubject.html', context)
 
 
-=======
->>>>>>> 91b04a5412e80f3a98e0d45fe4c4e84be1774a7d
