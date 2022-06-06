5cf2a57d13b8125dd4550f65c61c6fb34358c371
kaushal289 <khanalkaushal9@gmail.com>
2022-06-06 14:16:41 +0545

    tests

--- student/tests.py
+++ student/tests.py
@@ -1,1 +1,21 @@
+from django.test import SimpleTestCase,TestCase,Client
+from django.urls import reverse,resolve
+from student.views import *
+# Create your tests here.
 
+class TestUrls(SimpleTestCase):
+    def test_case_studentsubject_url(self):
+        url=reverse('studentsubject')
+        self.assertEqual(resolve(url).func,studentsubject)
+
+
+
+class TestViews(TestCase):
+    def setUp(self):
+        self.client=Client()
+
+    def test_studentsubject_GET(self):
+        response=self.client.get(reverse('studentsubject'))
+        self.assertEquals(response.status_code,200)
+        self.assertTemplateUsed(response,'student/studentsubject.html')
+        
--- teacher/tests.py
+++ teacher/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
-
+from django.test import SimpleTestCase,TestCase,Client
+from django.urls import reverse,resolve
+from teacher.views import *
 # Create your tests here.
+
+class TestUrls(SimpleTestCase):
+    def test_case_addsubject_url(self):
+        url=reverse('addsubject')
+        self.assertEqual(resolve(url).func,addsubject)
+
+
+
+
+class TestViews(TestCase):
+    def setUp(self):
+        self.client=Client()
+
+    def test_addsubject_GET(self):
+        response=self.client.get(reverse('addsubject'))
+        self.assertEquals(response.status_code,200)
+        self.assertTemplateUsed(response,'teacher/addsubject.html')
+
