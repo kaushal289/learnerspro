9705cfee3cdd9cfa5772f4e89b804e4650edb8b7
rujabhatta22 <rujabhatta22@gmail.com>
2022-06-16 21:23:30 +0545

    tearcherprofile

--- teacher/urls.py
+++ teacher/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('teacherdashboard',views.teacherdashboard,name="teacherdashboard"),
     path('teachersubject', views.teachersubject, name="teachersubject"),
     path('addsubject/', views.addsubject, name='addsubject'),
+    path('teacherprofile/<int:s_id>',views.teacherprofile,name="teacherprofile"),
     path('logout',views.logout,name='logout'),
 ]
--- teacher/views.py
+++ teacher/views.py
@@ -25,10 +25,33 @@ def logout(request):
     return redirect('/')
 
 
-
 def addsubject(request):
     if request.method == "POST":
         form = teachersubject(request.POST, request.FILES)
         form.save()
         return redirect("/teachersubject")
     return render(request, "teacher/addsubject.html")
+
+def teacherprofile(request,s_id):
+    try:
+        users=Teacher.objects.get(teacher_id=s_id)
+        return render(request,"teacher/teacherprofile.html",{'users':[users]})
+    except:
+        print("No Data Found")
+    return render(request,"Teacher/teacherprofile.html",{'users':[users]})
+
+def teacherprofile(request,s_id):
+    student=Teacher.objects.get(Teacher_id=s_id)
+    if request.method=="POST":
+        try:
+            if len(request.FILES) != 0:
+                if len(Teacher.image)>0:
+                    os.remove(teacher.image.path)
+                teacher.image=request.FILES['image']
+        except:
+            teacher.image=request.FILES['image']
+    form=TeacherForm(request.POST, instance=Teacher)
+    form.save()
+    request.session['username']=request.POST['username']
+    users=Teacher.objects.get(Teacher_id=s_id)
+    return render(request,"teacher/teacherprofile.html",{'users':[users]})
