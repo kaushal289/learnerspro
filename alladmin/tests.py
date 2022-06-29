995825de343be4588aaa219481460c43c60f0e63
kaushal289 <khanalkaushal9@gmail.com>
2022-06-29 19:14:25 +0545

    test

--- alladmin/tests.py
+++ alladmin/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
-
+from django.test import SimpleTestCase,TestCase,Client
+from django.urls import reverse,resolve
+from alladmin.views import *
 # Create your tests here.
+
+class TestUrls(SimpleTestCase):
+    def test_case_admindash_url(self):
+        url=reverse('admindash')
+        self.assertEqual(resolve(url).func,admindash)
+
+    def test_case_addteacher_url(self):
+        url=reverse('addteacher')
+        self.assertEqual(resolve(url).func,addteacher)
+
+
+class TestViews(TestCase):
+    def setUp(self):
+        self.client=Client()
+
+    def test_admindash_GET(self):
+        response=self.client.get(reverse('admindash'))
+        self.assertEquals(response.status_code,200)
+        self.assertTemplateUsed(response,'admindashboard.html')
+
+    def test_addteacher_GET(self):
+        response=self.client.get(reverse('addteacher'))
+        self.assertEquals(response.status_code,200)
+        self.assertTemplateUsed(response,'admin/addteacher.html')
