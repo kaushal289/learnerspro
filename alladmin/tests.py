035188fa8a1f40741753fc49540eae5cfaa67e58
kaushal289 <khanalkaushal9@gmail.com>
2022-07-04 22:06:09 +0545

    test sprint8

--- alladmin/tests.py
+++ alladmin/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
-
+from django.test import SimpleTestCase,TestCase,Client
+from django.urls import reverse,resolve
+from alladmin.views import *
 # Create your tests here.
+
+class TestUrls(SimpleTestCase):
+    def test_case_studentview_url(self):
+        url=reverse('studentview')
+        self.assertEqual(resolve(url).func,studentview)
+
+
+class TestViews(TestCase):
+    def setUp(self):
+        self.client=Client()
+
+    def test_admindash_GET(self):
+        response=self.client.get(reverse('studentview'))
+        self.assertEquals(response.status_code,200)
+        self.assertTemplateUsed(response,'admin/viewstudent.html')
